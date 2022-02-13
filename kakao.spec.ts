77c531fada092de229a048f0e9fb117aab7d2735
junkune <49082799+hwyangenhwa@users.noreply.github.com>
2022-02-13 12:21:52 +0900

    Ver (0.0.1)
    카카오톡 쇼핑관련 beforeEach 구문 설정 및 헤더정보 추출

--- kakao.spec.ts
+++ kakao.spec.ts
@@ -1,11 +1,18 @@
 import { test } from "@playwright/test";
+import { DCT_ROWS } from "opencv4nodejs";
 
-test('kakaotalkShoping ', async ({ page }) => {
-  await page.goto(
-    'https://store.kakao.com/',
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://store.kakao.com/',
     {
       timeout : 3000,
       waitUntil : "domcontentloaded"
-    }
-    );}
-);
+    });
+  });
+
+test('kakaoShoping Header Check', async ({ page }) => {
+  const kakaoShopHeader = await page.locator("ul[class='list_gnb'] >> li")
+  const count = await kakaoShopHeader.count()
+  
+  for (let i=0; i < count; ++i)
+    console.log(await kakaoShopHeader.nth(i).textContent());
+});
